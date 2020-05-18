e1a619a05a10e13b2237c3b6834d5638ab0c8dd6
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-05-19 09:05:40 +1000

    nav.js edited

--- src/nav.js
+++ src/nav.js
@@ -7,16 +7,11 @@
   
   document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
 
-// jQuery which slides the nav up and down when the burger icon is clicked
-
-//$(document).ready(function () {
-//  $(".fa-bars").click(() => {
-//    $("nav").slideToggle("slow");
-//  });
-//});
 // This code is from stack overflow, just wanted a way to slide the nav up and down when clicked.... My jQuery knowledge is still small
+
 $(document).ready(function () {
-  $('.fa-bars').click(function () {
-      $("nav").slideToggle("slow");
-  })
+$(".fa-bars").click(() => {
+$("nav").slideToggle("slow");
+ });
 });
+
