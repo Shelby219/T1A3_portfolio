c2224ac5d30c2d276fafdbe4b62f1c3139c26f89
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-05-19 09:10:13 +1000

    nav.js fixed

--- src/nav.js
+++ src/nav.js
@@ -9,8 +9,8 @@
 
 // This code is from stack overflow, just wanted a way to slide the nav up and down when clicked.... My jQuery knowledge is still small
 
-//$(document).ready(function () {
-//  $('.fa-bars').click(function () {
-//      $('.Navbar__Link-toggle').slideToggle("slow");
-//  })
-//}); 
+$(document).ready(function () {
+  $('.fa-bars').click(function () {
+      $('.Navbar__Link-toggle').slideToggle("slow");
+  })
+}); 
