c336694061320cda57baf62df8f1ea3cf58dd49e
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-05-19 09:02:25 +1000

    nav.js edited

--- src/nav.js
+++ src/nav.js
@@ -1,4 +1,5 @@
-function classToggle() {
+// This code is from stack overflow, found this was a good way to have a burger nav bar...
+ function classToggle() {
     const navs = document.querySelectorAll('.Navbar__Items')
     
     navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
@@ -6,3 +7,16 @@ function classToggle() {
   
   document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
 
+// jQuery which slides the nav up and down when the burger icon is clicked
+
+//$(document).ready(function () {
+//  $(".fa-bars").click(() => {
+//    $("nav").slideToggle("slow");
+//  });
+//});
+// This code is from stack overflow, just wanted a way to slide the nav up and down when clicked.... My jQuery knowledge is still small
+$(document).ready(function () {
+  $('.fa-bars').click(function () {
+      $("nav").toggleClass('slow');
+  })
+});
