a3b483558462115f87b6e3279cc9f23e53385dda
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-05-18 17:01:37 +1000

    nav.js edited

--- src/nav.js
+++ src/nav.js
@@ -6,10 +6,3 @@ function classToggle() {
   
   document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
 
-  // jQuery which slides the nav up and down when the burger icon is clicked
-
-$(document).ready(function () {
-    $(".fa-bars").click(() => {
-      $("nav").slideToggle("slow");
-    });
-  });
